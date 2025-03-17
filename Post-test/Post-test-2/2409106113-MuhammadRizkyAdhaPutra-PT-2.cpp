4b11c35d2f066c3fc5ef46721ee6ce7fbea4d4d0
unknown <rizky75338@gmail.com>
2025-03-17 15:06:34 +0800

    Finish Post Test 2

--- Post-test/Post-test-2/2409106113-MuhammadRizkyAdhaPutra-PT-2.cpp
+++ Post-test/Post-test-2/2409106113-MuhammadRizkyAdhaPutra-PT-2.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
 int main() {
-    // **Deklarasi Array**
+    // *Deklarasi Data Film*
     string judul[100] = {"Inception", "Titanic", "Avengers", "The Matrix", "Interstellar"};
     string genre[100] = {"Sci-Fi", "Romance", "Action", "Sci-Fi", "Sci-Fi"};
     int usia[100] = {13, 12, 13, 15, 13};
-    int filmC = 5; 
+    int filmC = 5; // Jumlah film saat ini
     bool Admin;
 
-    while (true) { 
+    // *Deklarasi Data Login dalam Array Multidimensi*
+    string akun[2][2] = {
+        {"admin", "admin123"},
+        {"Muhammad Rizky Adha Putra", "2409106113"}
+    };
+
+    while (true) {
         int role;
         cout << "\n+----------------------+" << endl;
-        cout << "|        APLIX        |" << endl;
+        cout << "|        APLIX         |" << endl;
         cout << "+----------------------+" << endl;
         cout << "| 1. Admin            |" << endl;
         cout << "| 2. User             |" << endl;
@@ -23,44 +30,45 @@ int main() {
         cin >> role;
         cin.ignore();
 
-        
-        if (role == 3) {  // Jika user memilih keluar
-            cout << "Terima Kasih Sudah Berkunjung Di APLIX." << endl;
+        if (role == 3) {
+            cout << "Terima Kasih Sudah Berkunjung Di APLIX.\n";
             return 0;
         }
-        
-        // **Login dengan batasan 3 kali percobaan**
+
+        // *Login dengan batasan 3 kali percobaan*
         bool loginSuccess = false;
         int attempts = 3;
-        
+
         while (attempts > 0) {
             string username, password;
-            cout << "Masukkan Username: ";
+            cout << "\nMasukkan Username: ";
             getline(cin, username);
             cout << "Masukkan Password: ";
             getline(cin, password);
 
-             // **Validasi Login**
-            if (role == 1 && username == "admin" && password == "admin123") {
+            // *Validasi Login*
+            if (role == 1 && username == akun[0][0] && password == akun[0][1]) {
                 Admin = true;
                 loginSuccess = true;
                 break;
-            } else if (role == 2 && username == "Muhammad Rizky Adha Putra" && password == "2409106113") {
+            } else if (role == 2 && username == akun[1][0] && password == akun[1][1]) {
                 Admin = false;
                 loginSuccess = true;
                 break;
             }
 
-
-            attempts--;  // Mengurangi jumlah percobaan
-            if (attempts > 0) {
-                cout << "User atau password anda salah! Sisa percobaan: " << attempts << endl;
-            } else {
-                cout << "Anda telah gagal login 3 kali. Program berhenti, Terima kasih sudah mengunjung APLIX." << endl;
-                return 0;
-            }
+            attempts--;
+            cout << "User atau password salah! Sisa percobaan: " << attempts << endl;
         }
 
+        if (!loginSuccess) {
+            cout << "Anda telah gagal login 3 kali. Program berhenti.\n";
+            return 0;
+        }
+
+        cout << "\nLogin berhasil! Selamat datang " << (Admin ? "Admin" : "User") << "!\n";
+
+        // *MENU ADMIN & USER*
         while (true) {
             int pilihan;
             cout << "\n+----------------------+" << endl;
@@ -82,7 +90,7 @@ int main() {
             cin.ignore();
 
             if (Admin) {
-                if (pilihan == 1) {  // Tambah film
+                if (pilihan == 1) {  // *Tambah Film*
                     if (filmC < 100) {
                         cout << "Masukkan Judul Film: ";
                         getline(cin, judul[filmC]);
@@ -92,25 +100,13 @@ int main() {
                         cin >> usia[filmC];
                         cin.ignore();
                         filmC++;
-                        cout << "Film berhasil ditambahkan!" << endl;
+                        cout << "Film berhasil ditambahkan!\n";
                     } else {
-                        cout << "Penyimpanan penuh!" << endl;
+                        cout << "Penyimpanan penuh!\n";
                     }
-
-
-                } else if (pilihan == 2) {  // Lihat film
-                    cout << "\n+-----------------------------------------------------+" << endl;
-                    cout << "| No |          Judul          |   Genre   |  Usia  |" << endl;
-                    cout << "+-----------------------------------------------------+" << endl;
-                    for (int i = 0; i < filmC; ++i) {
-                        cout << "| " << setw(2) << i + 1 << " | " << setw(20) << left << judul[i] << " | " << setw(10) << genre[i] << " | " << setw(5) << usia[i] << " |" << endl;
-                    }
-                    cout << "+-----------------------------------------------------+\n" << endl;
-
-
-                } else if (pilihan == 3) {  // Ubah film
+                } else if (pilihan == 2) {  // *Lihat Film*
                     if (filmC == 0) {
-                        cout << "Film belum tersedia." << endl;
+                        cout << "Belum ada film yang tersedia.\n";
                     } else {
                         cout << "\n+-----------------------------------------------------+" << endl;
                         cout << "| No |          Judul          |   Genre   |  Usia  |" << endl;
@@ -118,11 +114,24 @@ int main() {
                         for (int i = 0; i < filmC; ++i) {
                             cout << "| " << setw(2) << i + 1 << " | " << setw(20) << left << judul[i] << " | " << setw(10) << genre[i] << " | " << setw(5) << usia[i] << " |" << endl;
                         }
-                        cout << "+-----------------------------------------------------+\n" << endl;
+                        cout << "+-----------------------------------------------------+\n";
+                    }
+                } else if (pilihan == 3) {  // *Ubah Film*
+                    if (filmC == 0) {
+                        cout << "Belum ada film yang tersedia.\n";
+                    } else {
+                        cout << "\n+-----------------------------------------------------+" << endl;
+                        cout << "| No |          Judul          |   Genre   |  Usia  |" << endl;
+                        cout << "+-----------------------------------------------------+" << endl;
+                        for (int i = 0; i < filmC; ++i) {
+                            cout << "| " << setw(2) << i + 1 << " | " << setw(20) << left << judul[i] << " | " << setw(10) << genre[i] << " | " << setw(5) << usia[i] << " |" << endl;
+                        }
+                        cout << "+-----------------------------------------------------+\n";
                         cout << "Masukkan nomor film yang ingin diubah: ";
                         int nomorfilm;
                         cin >> nomorfilm;
                         cin.ignore();
+
                         if (nomorfilm > 0 && nomorfilm <= filmC) {
                             cout << "Masukkan judul baru: ";
                             getline(cin, judul[nomorfilm - 1]);
@@ -131,17 +140,14 @@ int main() {
                             cout << "Masukkan usia minimal baru: ";
                             cin >> usia[nomorfilm - 1];
                             cin.ignore();
-                            cout << "Film berhasil diubah!" << endl;
+                            cout << "Film berhasil diubah!\n";
                         } else {
-                            cout << "Nomor film tidak valid!" << endl;
+                            cout << "Nomor film tidak valid!\n";
                         }
                     }
-
-
-
-                } else if (pilihan == 4) {  // Hapus film
+                } else if (pilihan == 4) {  // *Hapus Film*
                     if (filmC == 0) {
-                        cout << "Film belum tersedia." << endl;
+                        cout << "Belum ada film yang tersedia.\n";
                     } else {
                         cout << "\n+-----------------------------------------------------+" << endl;
                         cout << "| No |          Judul          |   Genre   |  Usia  |" << endl;
@@ -149,11 +155,12 @@ int main() {
                         for (int i = 0; i < filmC; ++i) {
                             cout << "| " << setw(2) << i + 1 << " | " << setw(20) << left << judul[i] << " | " << setw(10) << genre[i] << " | " << setw(5) << usia[i] << " |" << endl;
                         }
-                        cout << "+-----------------------------------------------------+\n" << endl;
+                        cout << "+-----------------------------------------------------+\n";
                         cout << "Masukkan nomor film yang ingin dihapus: ";
                         int nomorfilm;
                         cin >> nomorfilm;
                         cin.ignore();
+
                         if (nomorfilm > 0 && nomorfilm <= filmC) {
                             for (int i = nomorfilm - 1; i < filmC - 1; ++i) {
                                 judul[i] = judul[i + 1];
@@ -161,34 +168,30 @@ int main() {
                                 usia[i] = usia[i + 1];
                             }
                             filmC--;
-                            cout << "Film yang anda pilih berhasil dihapus!" << endl;
+                            cout << "Film berhasil dihapus!\n";
                         } else {
-                            cout << "Nomor film tidak valid!" << endl;
+                            cout << "Nomor film tidak valid!\n";
                         }
                     }
-                } else if (pilihan == 5) {  // Logout
-                    cout << "Logout berhasil!" << endl;
+                } else if (pilihan == 5) {  // *Logout*
+                    cout << "Logout berhasil!\n";
                     break;
                 } else {
-                    cout << "Pilihan tidak valid!" << endl;
+                    cout << "Pilihan tidak valid!\n";
                 }
-            } else {  // Menu User
-                if (pilihan == 1) {  // Lihat film
-                    cout << "\n+-----------------------------------------------------+" << endl;
-                    cout << "| No |          Judul          |   Genre   |  Usia  |" << endl;
-                    cout << "+-----------------------------------------------------+" << endl;
+            } else {  // *User Menu*
+                if (pilihan == 1) {  // *Lihat Film*
+                    cout << "Daftar Film:\n";
                     for (int i = 0; i < filmC; ++i) {
-                        cout << "| " << setw(2) << i + 1 << " | " << setw(20) << left << judul[i] << " | " << setw(10) << genre[i] << " | " << setw(5) << usia[i] << " |" << endl;
+                        cout << i + 1 << ". " << judul[i] << " (" << genre[i] << ", " << usia[i] << "+)\n";
                     }
-                    cout << "+-----------------------------------------------------+\n" << endl;
-                } else if (pilihan == 2) {  // Logout
-                    cout << "Logout berhasil!" << endl;
+                } else if (pilihan == 2) {  // *Logout*
+                    cout << "Logout berhasil!\n";
                     break;
                 } else {
-                    cout << "Pilihan tidak valid!" << endl;
+                    cout << "Pilihan tidak valid!\n";
                 }
             }
         }
     }
-    return 0;
 }
